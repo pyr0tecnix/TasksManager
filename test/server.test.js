76b6a3985480b7f0a5dc1fb1b5b1677dda8d334c
Pyrotecnix <contact@vieyra.fr>
2017-09-27 16:42:08 +0200

    Add test on the schema of the json object returned

--- test/server.test.js
+++ test/server.test.js
@@ -1,17 +1,54 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var chaiJsonSchema = require('chai-json-schema');
 var server = require('./../src/server');
 var expect = chai.expect;
 
 chai.use(chaiHttp);
-
+chai.use(chaiJsonSchema);
 
 describe('GET TasksListOfTheDay', function(){
-  it('GET should return json object', function(){
-    chai.request(server)
-    .get('/TasksListOfTheDay')
-    .end((err, res) => {
+  let taskJsonSchema = {
+    title: 'Task schema',
+    type: 'object',
+    required: ['id', 'name', 'status', 'due_date'],
+    properties: {
+      id: {
+        type:'integer',
+        minimum: 0
+      },
+      name: {
+        type: 'string'
+      },
+      status: {
+        type: 'integer'
+      },
+      due_date: {
+        type: 'date'
+      }
+    }
+  };
+  it('GET should return with 200 status', (done) => {
+    chai.request(server).get('/TasksListOfTheDay').end((err, res) => {
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+  it('GET should return with application/json header', (done) => {
+    chai.request(server).get('/TasksListOfTheDay').end((err, res) => {
+      expect(res).to.have.header('content-type', 'application/json');
+      done();
+    });
+  });
+  it('GET should return json object', (done) => {
+    chai.request(server).get('/TasksListOfTheDay').end((err, res) => {
       expect(res).to.be.json;
+      done();
+    });
+  });
+  it('GET should return json object with the good schema', (done) => {
+    chai.request(server).get('/TasksListOfTheDay').end((err, res) => {
+      expect(res).to.be.jsonSchema(taskJsonSchema);
       done();
     });
   });
