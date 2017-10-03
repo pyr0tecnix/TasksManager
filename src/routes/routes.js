5670cdeccc066a15fab425579e9b848e88bbd048
Pyrotecnix <contact@vieyra.fr>
2017-10-03 16:17:39 +0200

    Add Body-parser in order to parse post param

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,19 @@
 let task = {'id': 0, 'name': 'Hello world', 'status': 2, 'due_date': Date.now()};
+let fs = require('fs');
+let bodyParser = require('body-parser');
+
 
 var apiRouter = (api) => {
+
+  api.use(bodyParser.json());
+
   api.get('/tasks', (req, res) => {
     res.setHeader('Content-type', 'application/json');
     res.status(200).send(JSON.stringify(task));
   });
   api.post('/tasks', (req, res) => {
     res.setHeader('Content-type', 'application/json');
+    console.log(req.body);
     res.status(201).send();
   });
 }
