4071977639a9a534ea85bd850a14d075f4edb46a
Pyrotecnix <contact@vieyra.fr>
2017-10-10 20:17:52 +0200

    GET taks return with json object with the right schema

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,7 @@
-let task = {'id': 0, 'name': 'Hello world', 'description': 'Foo Bar', 'status': 2, 'due_date': Date.now()};
+let task_1 = {'id': 0, 'name': 'Hello world', 'description': 'Foo Bar', 'status': 2, 'due_date': Date.now()};
+let task_2 = {'id': 1, 'name': 'Citation', 'description': 'There is no God up there', 'status': 3, 'due_date': Date.now()};
+let tasks = [task_1, task_2];
+
 let fs = require('fs');
 let bodyParser = require('body-parser');
 
@@ -9,12 +12,12 @@ var apiRouter = (api) => {
 
   api.get('/tasks', (req, res) => {
     res.setHeader('Content-type', 'application/json');
-    res.status(200).send('');
+    res.status(200).send(JSON.stringify(tasks));
   });
 
   api.get('/tasks/:id', (req, res) => {
     res.setHeader('Content-type', 'application/json');
-    res.status(200).send(JSON.stringify(task));
+    res.status(200).send(JSON.stringify(task_1));
   });
   api.post('/tasks', (req, res) => {
     let obj = {table: []};
